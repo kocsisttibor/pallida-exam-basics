7fdb5bff01e1272b2b46a3f9bc23561d1fd59e1b
Kocsis <kocsisttibor@gmail.com>
2017-10-06 09:52:01 +0200

    feat(exam-basics): unique_characters function created

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -1,6 +1,13 @@
 # Create a function called `unique_characters` that takes a string as parameter
 # and returns a list with the unique letters of the given string
 # Create basic unit tests for it with at least 3 different test cases
-print(unique_characters("anagram"))
+# print(unique_characters("anagram"))
 # Should print out:
 # ["n", "g", "r", "m"]
+
+def unique_characters(text):
+    uniques = []
+    for letter in set(list(text)):
+        if list(text).count(letter) == 1:
+            uniques.append(letter)
+    return uniques
